a32107b6e5f345bddbf0080216033f18839996f3
Nastya808 <nastyaergenova@icloud.com>
2024-09-08 16:06:03 +0300

    3

---
+++ dist/bill-gates-article/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-WGEBGTEC.mjs";import"./chunk-KM5JHQQP.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { QuotesComponent } from './quotes/quotes.component';
-import { BiographyComponent } from './biography/biography.component';
-import { ResourcesComponent } from './resources/resources.component';
+import { NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, FormsModule, QuotesComponent, BiographyComponent, ResourcesComponent],
+  imports: [CommonModule, FormsModule, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'bill-gates-article';
+
+  currentView: string = 'biography'; 
 
   quotes: string[] = [
     "Patience is a key element of success",
@@ -22,7 +22,7 @@ export class AppComponent {
     "Success is a lousy teacher. It seduces smart people into thinking they can’t lose",
     "Be nice to nerds. Chances are you’ll end up working for one"
   ];
-  
+
   resources: { name: string, url: string }[] = [
     { name: 'Bill Gates on Wikipedia', url: 'https://en.wikipedia.org/wiki/Bill_Gates' },
     { name: 'Bill & Melinda Gates Foundation', url: 'https://www.gatesfoundation.org/' },
@@ -33,6 +33,10 @@ export class AppComponent {
   newQuote: string = '';
   newResourceName: string = '';
   newResourceUrl: string = '';
+
+  showView(view: string) {
+    this.currentView = view;
+  }
 
   addQuote() {
     if (this.newQuote) {
